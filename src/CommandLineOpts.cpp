d2c2bca993324e29425d534876f32f1ad0718ba8
Pradeep Kumar <pradeepisro49@gmail.com>
2019-08-09 09:27:51 +0530

    [SPMDFY] Separated out commandline options to make it more manageable

--- src/CommandLineOpts.cpp
+++ src/CommandLineOpts.cpp
@@ -10,8 +10,12 @@ llvm::cl::opt<bool>
     dump_json("dump-json",
               llvm::cl::desc("Jump JSON description of the Translation Unit"),
               llvm::cl::cat(spmdfy_options));
-              
+
 llvm::cl::opt<bool>
     verbosity("v",
               llvm::cl::desc("Show commands to run and use verbose output"),
               llvm::cl::value_desc("v"), llvm::cl::cat(spmdfy_options));
+
+llvm::cl::opt<std::string>
+    ispc_macro("ispc_macro", llvm::cl::desc("File containing ISPC Macros"),
+               llvm::cl::cat(spmdfy_options));
