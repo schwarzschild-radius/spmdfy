db557d5e8d30444951dd128c01604cb52fa862df
Pradeep Kumar <pradeepisro49@gmail.com>
2019-08-22 08:34:20 +0530

    [SPMDFY/PASS] Updated PrintCFG pass

--- src/Pass/Passes/PrintCFGPass.cpp
+++ src/Pass/Passes/PrintCFGPass.cpp
@@ -4,13 +4,15 @@ namespace spmdfy {
 
 namespace pass {
 
-bool print_cfg_pass(SpmdTUTy& spmd_tu, clang::ASTContext& ast_context, Workspace& workspace) {
+bool print_cfg_pass(SpmdTUTy &spmd_tu, clang::ASTContext &ast_context,
+                    Workspace &workspace) {
     for (auto node : spmd_tu) {
-        SPMDFY_INFO("Visiting Node {}", node->getNodeTypeName());
-        if(node->getNodeType() == cfg::CFGNode::KernelFunc){
-            cfg::CFGNode* curr_node = node;
-            while(curr_node->getNodeType() != cfg::CFGNode::Exit){
-                SPMDFY_INFO("Visiting Node {}", curr_node->getName());
+        SPMDFY_INFO("[PrintCFGPass] Visiting Node {}", node->getNodeTypeName());
+        if (node->getNodeType() == cfg::CFGNode::KernelFunc) {
+            cfg::CFGNode *curr_node = node;
+            while (curr_node->getNodeType() != cfg::CFGNode::Exit) {
+                SPMDFY_INFO("[PrintCFGPass] Visiting Node {}, {}",
+                            curr_node->getSource(), curr_node->getNodeTypeName());
                 curr_node = curr_node->getNext();
             }
         }
